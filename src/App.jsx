246412824642346404ade5ad704c18c92f5ea42e
ehsan <ehsansaraee6570@yahoo.com>
2022-01-30 11:07:31 +0330

    routing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar, Sidebar, Footer } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar } from "./components";
+import {
+   About,
+   Cart,
+   Checkout,
+   Error,
+   Home,
+   Products,
+   SingleProduct,
+} from "./Pages";
 
 const App = () => {
-   return <h4>comfy sloth starter</h4>;
+   return (
+      <BrowserRouter>
+         <Navbar />
+         <Sidebar />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<SingleProduct />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="*" element={<Error />} />
+         </Routes>
+      </BrowserRouter>
+   );
 };
 
 export default App;
