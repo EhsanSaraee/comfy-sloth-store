e77049e5b9cabff5fa861caab03cedec8a42f15f
ehsan <ehsansaraee6570@yahoo.com>
2022-01-30 19:13:48 +0330

    sidebar section added

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -8,7 +8,30 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../Context/user_context";
 
 const Sidebar = () => {
-   return <h4>sidebar</h4>;
+   const isOpen = true;
+   return (
+      <SidebarContainer>
+         <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+            <div className="sidebar-header">
+               <img src={logo} className="logo" alt="Comfy Sloth" />
+               <button type="button" className="close-btn">
+                  <FaTimes />
+               </button>
+            </div>
+            <ul className="links">
+               {links.map(({ id, text, url }) => (
+                  <li key={id}>
+                     <Link to={url}>{text}</Link>
+                  </li>
+               ))}
+               <li>
+                  <Link to="/checkout">Checkout</Link>
+               </li>
+            </ul>
+            <CartButtons />
+         </aside>
+      </SidebarContainer>
+   );
 };
 
 const SidebarContainer = styled.div`
