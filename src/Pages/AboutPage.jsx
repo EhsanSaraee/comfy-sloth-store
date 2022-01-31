5d74c62f40219f7b270ebb17fe393ad77d417feb
ehsan <ehsansaraee6570@yahoo.com>
2022-01-31 05:50:36 +0330

    about & checkout page development

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -3,7 +3,26 @@ import { PageHero } from "../Components";
 import aboutImg from "../Assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-   return <h4>about page</h4>;
+   return (
+      <main>
+         <PageHero title='about' />
+         <Wrapper className="page section section-center">
+            <img src={aboutImg} alt="nice desk" />
+            <article>
+               <div className="title">
+                  <h2>our story</h2>
+                  <div className="underline" />
+               </div>
+               <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
+                  quidem voluptas modi suscipit! Harum voluptatem, alias velit
+                  fuga ipsa perspiciatis doloribus commodi eum nostrum officia
+                  dolorem ut cum, dolorum earum.
+               </p>
+            </article>
+         </Wrapper>
+      </main>
+   );
 };
 
 const Wrapper = styled.section`
