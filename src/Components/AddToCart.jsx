c73322f8c723d8ff4c655ca815e8f04db025ba30
ehsan <ehsansaraee6570@yahoo.com>
2022-02-06 11:25:15 +0330

    add to cart amount buttons logic

--- src/Components/AddToCart.jsx
+++ src/Components/AddToCart.jsx
@@ -7,7 +7,29 @@ import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
    const { id, stock, colors } = product;
+
    const [mainColor, setMainColor] = useState(colors[0]);
+   const [amount, setAmount] = useState(1);
+
+   const increment = () => {
+      setAmount((prevAmount) => {
+         let tempAmount = prevAmount + 1;
+         if (tempAmount > stock) {
+            tempAmount = stock;
+         }
+         return tempAmount;
+      });
+   };
+
+   const decrement = () => {
+      setAmount((prevAmount) => {
+         let tempAmount = prevAmount - 1;
+         if (tempAmount < 1) {
+            tempAmount = 1;
+         }
+         return tempAmount;
+      });
+   };
 
    return (
       <Wrapper>
@@ -28,7 +50,16 @@ const AddToCart = ({ product }) => {
                ))}
             </div>
          </div>
-         <div className="btn-container"></div>
+         <div className="btn-container">
+            <AmountButtons
+               amount={amount}
+               increment={increment}
+               decrement={decrement}
+            />
+            <Link to="/cart" className="btn">
+               add to cart
+            </Link>
+         </div>
       </Wrapper>
    );
 };
