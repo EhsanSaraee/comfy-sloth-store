1ae17d5ff132146074282eefb0909289a0ac9e8a
ehsan <ehsansaraee6570@yahoo.com>
2022-02-08 13:13:09 +0330

    calculate totals

--- src/Components/CartButtons.jsx
+++ src/Components/CartButtons.jsx
@@ -1,19 +1,20 @@
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { useProductsContext } from "../Context/products_context";
-import { useCartContext } from "../Context/cart_context";
-import { useUserContext } from "../Context/user_context";
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useProductsContext } from '../Context/products_context';
+import { useCartContext } from '../Context/cart_context';
+import { useUserContext } from '../Context/user_context';
 
 const CartButtons = () => {
    const { closeSidebar } = useProductsContext();
+   const { total_items } = useCartContext();
    return (
       <Wrapper className="cart-btn-wrapper">
          <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
             Cart
             <span className="cart-container">
                <FaShoppingCart />
-               <span className="cart-value">12</span>
+               <span className="cart-value">{total_items}</span>
             </span>
          </Link>
          <button type="button" className="auth-btn">
